c86f89535c03e8c4951f7063cca3290473152ab0
Jimena Ruiz <jyellow363@gmail.com>
2025-01-04 23:52:11 -0500

    Template Project

---
+++ src/main/java/com/lmp/demoJenkinsSonar/DemoJenkinsSonarApplication.java
@@ -0,0 +1,13 @@
+package com.lmp.demoJenkinsSonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJenkinsSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJenkinsSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmp/demoJenkinsSonar/DemoJenkinsSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmp.demoJenkinsSonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
